99ff6c69aa323c84e0cb722f4a5305d81873432f
Sriya Cheruku <sriyacheruku@Sriyas-MacBook-Air.local>
2026-05-17 23:30:01 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+export default function App() {
+  return (
+    <div className="page">
+     <main className="card">
+      
+      <img src="/images/avatar-jessica.jpeg" alt="essica Randall"  className="avatar"/> 
+      <h1 className="name">Jessica randall</h1>
+      <p className="location">London, United kingdom</p>
+      <p className="bio">"Front-end developer and avid reader."</p>
+      <ul className="social-links">
+        <li><a href="#" className="social-link">GitHub</a></li>
+        <li><a href="#" className="social-link">Frontend Mentor</a></li>
+        <li><a href="#" className="social-link">LinkedIn</a></li>
+        <li><a href="#" className="social-link">Twitter</a></li>
+        <li><a href="#" className="social-link">Instagram</a></li>
+      </ul>
+     </main>
+    </div>
+  )
+}
---
+++ src/components/SocialCard.jsx
@@ -0,0 +1,9 @@
+function SocialCard() {
+  return (
+    <div>
+      Social Card
+    </div>
+  )
+}
+
+export default SocialCard
