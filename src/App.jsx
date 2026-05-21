05f1b34d072ed0dcced4cb15bad22cbcc69528e7
Sriya Cheruku <sriyacheruku@Sriyas-MacBook-Air.local>
2026-05-22 00:01:24 +0530

    updated UI changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ export default function App() {
     <div className="page">
      <main className="card">
       
-      <img src="/images/avatar-jessica.jpeg" alt="essica Randall"  className="avatar"/> 
-      <h2 className="name">Jessica randall</h2>
+      <img src="/images/avatar-jessica.jpeg" alt="essica Randall "  className="avatar"/> 
+      <h3 className="name">Jessica randall</h3>
       <h4 className="location">London, United kingdom</h4>
       <h5 className="bio">"Front-end developer and avid reader."</h5>
       <ul className="social-links">
